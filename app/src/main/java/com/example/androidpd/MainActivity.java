f998e9242c7cba6ad394cb25d07da7076173a424
andrewkibet <kibetarwa@gmail.com>
2022-12-21 10:54:15 +0300

    Adding code to onClick

--- app/src/main/java/com/example/androidpd/MainActivity.java
+++ app/src/main/java/com/example/androidpd/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.androidpd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private static final int pic_id =123;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Start the activity with camera_intent, and request pic id
+                startActivityForResult(camera_intent, pic_id);
 
             }
         });
