b5ec31f0fab64633c32c87bccbc4612f0333e525
andrewkibet <kibetarwa@gmail.com>
2022-12-21 09:20:40 +0300

    Edit button code. Java

--- app/src/main/java/com/example/androidpd/MainActivity.java
+++ app/src/main/java/com/example/androidpd/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.androidpd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.photo);
     }
+
 }
