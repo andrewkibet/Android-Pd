1e672835f03896eef237d88ab4765dd51fa1f3c2
andrewkibet <kibetarwa@gmail.com>
2022-12-22 17:05:09 +0300

    Delete button onClickListener.

--- app/src/main/java/com/example/androidpd/MainActivity.java
+++ app/src/main/java/com/example/androidpd/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    Button button;
+    ImageView click;
     private static final int pic_id =123;
 
     @SuppressLint("MissingInflatedId")
@@ -19,15 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.photo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                // Start the activity with camera_intent, and request pic id
-                startActivityForResult(camera_intent, pic_id);
-
-            }
-        });
+        click = findViewById(R.id.imageView);
 
 
     }
