02280a733406d928bc60593f78f35b5dc09a9137
andrewkibet <kibetarwa@gmail.com>
2023-01-01 22:20:26 +0300

    Create phonecall.java

---
+++ app/src/main/java/com/example/androidpd/phonecall.java
@@ -0,0 +1,14 @@
+package com.example.androidpd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class phonecall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonecall);
+    }
+}
