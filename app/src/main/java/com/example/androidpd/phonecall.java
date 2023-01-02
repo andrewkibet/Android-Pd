fa7d901915b7e98bd173cdaec1f035942444507f
andrewkibet <kibetarwa@gmail.com>
2023-01-02 22:07:44 +0300

    onlclick Method

--- app/src/main/java/com/example/androidpd/phonecall.java
+++ app/src/main/java/com/example/androidpd/phonecall.java
@@ -3,12 +3,23 @@ package com.example.androidpd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class phonecall extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phonecall);
+
+        Button startBtn = (Button) findViewById(R.id.makecall);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
