30e10f03d369457809ffecd491de06835180ecea
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-01-26 23:01:37 +0300

    Update Allapps.java

--- app/src/main/java/com/example/androidpd/Allapps.java
+++ app/src/main/java/com/example/androidpd/Allapps.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class Allapps extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allapps);
+        button = findViewById(R.id.bn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+                mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+                final List pkgAppsList = context.getPackageManager().queryIntentActivities( mainIntent, 0);
+            }
+        });
     }
 
     public void BtnListapps(View view) {
